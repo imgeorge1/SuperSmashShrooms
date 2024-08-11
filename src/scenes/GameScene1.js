1cc0f765f9eaf0f2056f5129e201883a7d8cd3ca
User <Ubitoz133@gmail.com>
2024-08-11 14:23:34 +0400

    improved movement

--- src/scenes/GameScene1.js
+++ src/scenes/GameScene1.js
@@ -1,14 +1,14 @@
 export default class GameScene1 extends Phaser.Scene {
   constructor() {
-      super('GameScene1');
+    super('GameScene1');
   }
 
-  
   create() {
     // Define class-level variables
     this.platforms;
     this.player1;
-    this.cursors;
+    this.keys; // Define keys here
+
     // Background
     this.add.image(600, 350, "sky").setScale(1.5);
     this.add.image(600, 100, "sun").setScale(0.1);
@@ -24,10 +24,9 @@ export default class GameScene1 extends Phaser.Scene {
     this.physics.add.collider(this.player1, this.platforms);
     this.player1.setBounce(0.15);
     this.player1.setCollideWorldBounds(false);
-    // this.player1.body.setCircle(10);
     this.player1.body.setSize(15, 28);
     this.player1.body.setOffset(23, -1);
-    
+
     // Create an invisible sprite to act as the second hitbox
     this.hitbox2 = this.physics.add.sprite(this.player1.x, this.player1.y, null);
     this.hitbox2.body.setSize(50, 10); // Set size of the secondary hitbox 
@@ -39,54 +38,55 @@ export default class GameScene1 extends Phaser.Scene {
     // Example: Collide the secondary hitbox with platforms
     this.physics.add.collider(this.hitbox2, this.platforms);
 
-
-    // Input handling
-    this.cursors = this.input.keyboard.createCursorKeys();
+    // Input handling for WASD keys
+    this.keys = this.input.keyboard.addKeys({
+      W: Phaser.Input.Keyboard.KeyCodes.W,
+      A: Phaser.Input.Keyboard.KeyCodes.A,
+      S: Phaser.Input.Keyboard.KeyCodes.S,
+      D: Phaser.Input.Keyboard.KeyCodes.D
+    });
 
     // Define Animations
     this.anims.create({
-        key: 'left',
-        frames: this.anims.generateFrameNumbers('p1', { start: 0, end: 3 }),
-        frameRate: 10,
-        repeat: -1
+      key: 'left',
+      frames: this.anims.generateFrameNumbers('p1', { start: 0, end: 3 }),
+      frameRate: 10,
+      repeat: -1
     });
 
     this.anims.create({
-        key: 'turn',
-        frames: [{ key: 'p1', frame: 4 }],
-        frameRate: 20
+      key: 'turn',
+      frames: [{ key: 'p1', frame: 4 }],
+      frameRate: 20
     });
 
     this.anims.create({
-        key: 'right',
-        frames: this.anims.generateFrameNumbers('p1', { start: 5, end: 8 }),
-        frameRate: 10,
-        repeat: -1
+      key: 'right',
+      frames: this.anims.generateFrameNumbers('p1', { start: 5, end: 8 }),
+      frameRate: 10,
+      repeat: -1
     });
   }
+
   update() {
     // Ensure hitbox2 follows player1 exactly
     this.hitbox2.setPosition(this.player1.x, this.player1.y);
 
-
-    if (this.cursors.left.isDown) {
-      this.player1.setVelocityX(-160);
+    if (this.keys.A.isDown) {
+      this.player1.setVelocityX(-180);
       this.player1.anims.play('left', true);
-    }
-    else if (this.cursors.right.isDown) {
-      this.player1.setVelocityX(160);
+    } else if (this.keys.D.isDown) {
+      this.player1.setVelocityX(180);
       this.player1.anims.play('right', true);
-    }
-    else {
+    } else {
       this.player1.setVelocityX(0);
       this.player1.anims.play('turn');
     }
 
-    if (this.cursors.up.isDown && this.player1.body.touching.down) {
+    if (this.keys.W.isDown && this.player1.body.touching.down) {
       this.player1.setVelocityY(-200);
+    }else  if (this.keys.S.isDown && !this.player1.body.touching.down) {
+      this.player1.setVelocityY(150);
     }
-
-   
   }
-
-  }
+}
